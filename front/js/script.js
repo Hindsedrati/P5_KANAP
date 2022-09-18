b7725c0fa6fedc6ad92f547a60c8b60b402a98a5
Hindsedrati <92885675+Hindsedrati@users.noreply.github.com>
2022-09-18 21:34:36 +0200

    Update script.js

--- front/js/script.js
+++ front/js/script.js
@@ -1,39 +1,32 @@
-function getProducts(){
-
+// Récupération des produits de l'api
 fetch("http://localhost:3000/api/products")
-.then(function(res) {
-    if (res.ok) {
-        return res.json();
-    }
+// quand tu as la réponse donne le résultat en json.
+.then((res) => res.json())
+// ce que l'on a reçu et qui a été traité en json sera appelé objetProduits
+.then((objetProduits) => {
+    // donne moi des informations en console sur ce qui est récupéré sous forme tableau.
+    console.table(objetProduits);
+    // appel de la fonction d'affichage des produits
+    kanapé(objetProduits);
 })
-.then(function(res) {
-    let s ="";
-    for(let product of res){
-        let id = product._id;
-        let nom = product.name;
-        let prix = product.price;
-        let color = product.colors;
-        let description = product.description;
-        let altTxt = product.altTxt;
-        let imageUrl = product.imageUrl
-        
-        s+= '<a href="./product.html?id='+id+'">'+
-        '<article>'+
-        '<img src="'+imageUrl+'" alt="'+altTxt+'">'+
-        '<h3 class="productName">'+nom+'</h3>'+
-        '<p class="productDescription">'+description+'</p>'+
-        //'<button id="color">'+color+'</button>'+
-        '</article>'+
-        '</a>';
-        
-    }
-    let section = document.getElementById('items');
-    section.innerHTML = s
-    console.log(s);
-    
-})
-
-.catch(function(err) {
-    // Une erreur est survenue
+// dans le cas d'une erreur remplace le contenu de titre par un h1 au contenu de erreur 404 et renvoit en console l'erreur.
+.catch((err) => {
+    document.querySelector(".titles").innerHTML = "<h1>erreur 404</h1>";
+    console.log("erreur 404, sur ressource api:" + err);
 });
+// fonction d'affichage des produits de l'api sur la page index
+
+function kanapé(index) {
+    // déclaration de variable de la zone d'article
+    let zoneArticle = document.querySelector("#items");
+    // boucle pour chaque indice(nommé 'article') dans index
+    for (let article of index) {
+        zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
+        <article>
+        <img src="${article.imageUrl}" alt="${article.altTxt}">
+        <h3 class="productName">${article.name}</h3>
+        <p class="productDescription">${article.description}</p>
+        </article>
+        </a>`;
+    }
 }
